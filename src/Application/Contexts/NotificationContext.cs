8036ec6bcdee40380dd6bbb61c86a768427ca6e1
kauatwn <kaualopescontato@gmail.com>
2024-09-24 17:06:02 -0300

    Criando NotificationContext

---
+++ src/Application/Contexts/NotificationContext.cs
@@ -0,0 +1,21 @@
+using Domain.Enums;
+using Domain.Interfaces.Factories;
+using Domain.Interfaces.Strategies;
+
+namespace Application.Contexts;
+
+public class NotificationContext(INotificationStrategy strategy, INotificationStrategyFactory strategyFactory)
+{
+    private INotificationStrategy Strategy { get; set; } = strategy;
+    private INotificationStrategyFactory StrategyFactory { get; } = strategyFactory;
+
+    public void SetStrategy(NotificationChannel channel)
+    {
+        Strategy = StrategyFactory.GetNotificationStrategy(channel);
+    }
+
+    public string Send(string sender, string recipient, string content)
+    {
+        return Strategy.Send(sender, recipient, content);
+    }
+}
